a951faa457dde83b6b9f9000b792c06f6d19fd18
zxwg6329@gmail.com <yuyaisaka87@gmail.com>
2021-04-17 03:31:41 +0900

    create loop & lexer

---
+++ Readme.md
@@ -0,0 +1,4 @@
+# Nimで自作Shellを書いてみよう
+> 参考：
+> - https://brennan.io/2015/01/16/write-a-shell-in-c/
+> - https://github.com/brenns10/lsh
--- main.nim
+++ main.nim
@@ -1,2 +1,54 @@
+import strutils
 
-echo "Hello"
+var line: string
+var args: seq[string]
+var status: int
+
+# do-whileテンプレート
+template doWhile(a, b: untyped): untyped =
+  b
+  while a:
+    b
+
+#-------------------------------------------------------
+
+# lexer
+proc tokenize() =
+    var i = 0
+    while len(line) > i:
+
+      # skip space
+      if isSpaceAscii(line[i]) or line[i] == '\t':
+        inc(i)
+        continue
+
+      # make string seq
+      var tmp = $line[i]
+      inc(i)
+      while len(line) > i:
+        if isSpaceAscii(line[i]) or line[i] == '\t':
+          break
+        tmp.add($line[i])
+        inc(i)
+      args.add(tmp)
+
+      inc(i)
+
+proc lshLoop() =
+
+  doWhile bool(status):
+    stdout.write "> "
+    line = readLine(stdin)
+    tokenize()
+
+    echo args
+
+#--------------------------------------------------------
+
+proc main() =
+
+  lshLoop()
+
+  quit(0)
+
+main()
